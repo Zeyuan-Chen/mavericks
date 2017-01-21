78719fc6fd3ccea39be8cd2ba5bf4ff5b0398982
Qianmeng Chen <qianmengchen@outlook.com>
2017-01-20 23:05:19 -0800

    DataBase set up

---
+++ credentials.js
@@ -0,0 +1,8 @@
+mongo: {
+    development: {
+        connectionString: 'mongodb://admin:sbhacksmavericks@ds117819.mlab.com:17819/mavericks',
+    },
+    production: {
+        connectionString: 'mongodb://admin:sbhacksmavericks@ds117819.mlab.com:17819/mavericks',
+    },
+},
