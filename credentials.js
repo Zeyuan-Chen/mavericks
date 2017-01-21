f03f0e36c4f905d3c5fa2996b0e44eb55f1ce1c1
Qianmeng Chen <qianmengchen@outlook.com>
2017-01-20 23:05:57 -0800

    cleanUpABit

--- credentials.js
+++
@@ -1,8 +0,0 @@
-mongo: {
-    development: {
-        connectionString: 'mongodb://admin:sbhacksmavericks@ds117819.mlab.com:17819/mavericks',
-    },
-    production: {
-        connectionString: 'mongodb://admin:sbhacksmavericks@ds117819.mlab.com:17819/mavericks',
-    },
-},
